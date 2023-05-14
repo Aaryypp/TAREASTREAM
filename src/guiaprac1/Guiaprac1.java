c24776d34fd4e8fd91322a1d968a5baf3889f711
Aaryypp <126633617+Aaryypp@users.noreply.github.com>
2023-05-14 11:05:05 -0500

    primer

---
+++ src/guiaprac1/Guiaprac1.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package guiaprac1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ariel
+ */
+public class Guiaprac1 {
+
+    /**
+     * @param args the command line arguments
+     */    public static void main(String[] args) {
+         
+         
+         
+         
+         
+//                ArrayList<Persona> listaperso = new ArrayList<>();
+//        try {
+//            File archivo = new File("src\\guiaprac1\\datos.txt");
+//            Scanner scanner = new Scanner(archivo);
+//            while (scanner.hasNextLine()) {
+//                String linea = scanner.nextLine();
+//                String[] datos = linea.split(" ");
+//                Persona persona = new Persona(datos[0], datos[1], datos[2]);
+//                System.out.println(persona.toString());
+//                listaperso.add(persona);
+//            }
+//           
+//        } catch (FileNotFoundException e) {
+//            System.out.println("Archivo no encontrado");
+//        }
+       
+        
+       
+    }
+    
+}
